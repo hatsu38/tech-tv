ec398f2b61cfa36f3913d7339e35df71b54aa93c
hatsu38 <hajiwata0308@gmail.com>
2020-07-11 18:56:24 +0900

    Fix @tags

--- app/controllers/api/v1/ranking/tags_controller.rb
+++ app/controllers/api/v1/ranking/tags_controller.rb
@@ -5,6 +5,6 @@ class Api::V1::Ranking::TagsController <ApplicationController
   def index
     tags_num = (params[:tags_num] || DEFAULT_GET_NUMS).to_i
     @events_num = (params[:events_num] || DEFAULT_GET_EVENTS_NUMS).to_i
-    @tags = params[:ranking] === 'related' ? Event.popular_event_tags(tags_num) : Tag.related_event_many_order(tags_num)
+    @tags = params[:ranking] === 'related' ? Tag.related_event_many_order(tags_num) : Event.popular_event_tags(tags_num)
   end
 end
