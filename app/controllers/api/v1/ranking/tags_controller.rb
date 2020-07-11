a46ae55eb1c83f71a450c82ead8c48afafdeea5b
hatsu38 <hajiwata0308@gmail.com>
2020-07-11 18:30:01 +0900

    Add EventsNum Paramater

--- app/controllers/api/v1/ranking/tags_controller.rb
+++ app/controllers/api/v1/ranking/tags_controller.rb
@@ -1,8 +1,10 @@
 class Api::V1::Ranking::TagsController <ApplicationController
-  DEFAULT_GET_NUMS = 10
+  DEFAULT_GET_TAGS_NUMS = 10
+  DEFAULT_GET_EVENTS_NUMS = 5
 
   def index
-    per_num = (params[:num] || DEFAULT_GET_NUMS).to_i
-    @tags = params[:ranking] === 'related' ? Event.popular_event_tags(per_num) : Tag.related_event_many_order(per_num)
+    tags_num = (params[:tags_num] || DEFAULT_GET_NUMS).to_i
+    @events_num = (params[:events_num] || DEFAULT_GET_EVENTS_NUMS).to_i
+    @tags = params[:ranking] === 'related' ? Event.popular_event_tags(tags_num) : Tag.related_event_many_order(tags_num)
   end
 end
