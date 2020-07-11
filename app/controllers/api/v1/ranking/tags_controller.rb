653dba3af4bc028fc818b34a8d700ca88ab21ffc
hatsu38 <hajiwata0308@gmail.com>
2020-07-11 14:11:25 +0900

    Add Ranking View With Cache

--- app/controllers/api/v1/ranking/tags_controller.rb
+++ app/controllers/api/v1/ranking/tags_controller.rb
@@ -2,6 +2,9 @@ class Api::V1::Ranking::TagsController <ApplicationController
   DEFAULT_GET_NUMS = 10
 
   def index
-    @tags = params[:ranking] === 'related' ? Event.popular_event_tags(DEFAULT_GET_NUMS) : Tag.related_event_many_order(DEFAULT_GET_NUMS)
+    per_num = (params[:num] || DEFAULT_GET_NUMS).to_i
+    Rails.cache.fetch("#{request.path}/#{per_num}/#{params[:ranking]}/index", expires_in: 10.minutes) do
+      @tags = params[:ranking] === 'related' ? Event.popular_event_tags(per_num) : Tag.related_event_many_order(per_num)
+    end
   end
 end
