1e68de1497c235364727411b26e8a0758e05567e
hatsu38 <hajiwata0308@gmail.com>
2020-07-18 17:38:22 +0900

    Fix Site Name

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Connpass Tube Api"
+  config.site_title = "こんぱすちゅーぶ"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
