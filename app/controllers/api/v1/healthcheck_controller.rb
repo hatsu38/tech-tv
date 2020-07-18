4f54d536efb005a28f5f57918224290de7bb42e7
hatsu38 <hajiwata0308@gmail.com>
2020-07-18 18:05:51 +0900

    Add HealthCheck

---
+++ app/controllers/api/v1/healthcheck_controller.rb
@@ -0,0 +1,3 @@
+class Api::V1::HealthcheckController < ApplicationController
+  include Healthcheckable
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: "events#index"
+  resources :healthcheck, only: [:index]
   resources :events, only: [:index, :show]
   resources :newly_events, only: [:index]
   resources :tags, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, { format: 'json' } do
     namespace :v1 do
+      resources :healthcheck, only: [:index]
       resources :events, only: [:index, :show]
       resources :newly_events, only: [:index]
       resources :tags, only: [:index, :show]
