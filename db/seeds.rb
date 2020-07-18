b32af3fcd03ce16bb0b51cc92025586ef2624129
hatsu38 <hajiwata0308@gmail.com>
2020-07-18 16:46:07 +0900

    Fix Seeds And Readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,24 +7,24 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-# system('bundle exec rails batch:save_event')
-# system('bundle exec rails batch:save_movie')
+system('bundle exec rails batch:save_event')
+system('bundle exec rails batch:save_movie')
 
-# Dir.glob("#{Rails.root}/db/seeds/*.yml").each do |yaml_filename|
-#   # yamlのファイル名から、対応するモデルクラスを特定し、クラスをロードする
-#   # 下記行が無いと、「ArgumentError: undefined class/module」が発生する。
-#   target_model = File.basename(yaml_filename,".yml").classify.constantize
+Dir.glob("#{Rails.root}/db/seeds/*.yml").each do |yaml_filename|
+  # yamlのファイル名から、対応するモデルクラスを特定し、クラスをロードする
+  # 下記行が無いと、「ArgumentError: undefined class/module」が発生する。
+  target_model = File.basename(yaml_filename,".yml").classify.constantize
 
-#   # yamlに記述されたレコードをDBに登録する。
-#   File.open(yaml_filename) do |load_target_yaml|
-#     records = YAML.load(load_target_yaml)
-#     records.each do |record|
-#       target_model.find_or_create_by!(record)
-#     end
-#   end
-# end
+  # yamlに記述されたレコードをDBに登録する。
+  File.open(yaml_filename) do |load_target_yaml|
+    records = YAML.load(load_target_yaml)
+    records.each do |record|
+      target_model.find_or_create_by!(record)
+    end
+  end
+end
 
-# system('bundle exec rails batch:save_event_tag')
+system('bundle exec rails batch:save_event_tag')
 
 AdminUser.create!(
   email: ENV['ADMIN_EMAIL'],
