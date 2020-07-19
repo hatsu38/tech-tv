3f2f7c8f7129b86471d15a89081c07809e58a3ce
hatsu38 <hajiwata0308@gmail.com>
2020-07-19 14:35:56 +0900

    Add Redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
 
+  before_filter :ensure_domain
+
+  FQDN = 'connpass-tube.hatsu38.com'
+  # redirect correct server from herokuapp domain for SEO
+  def ensure_domain
+    return unless /\.herokuapp.com/ =~ request.host
+
+    # 主にlocalテスト用の対策80と443以外でアクセスされた場合ポート番号をURLに含める
+    port = ":#{request.port}" unless [80, 443].include?(request.port)
+    redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}", status: :moved_permanently
+  end
+
   if Rails.env.production?
     rescue_from StandardError, with: :rescue_500
     rescue_from ActionController::RoutingError, with: :rescue_404
