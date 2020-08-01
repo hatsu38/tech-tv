05b352222bf2b1836b791624b8f21c84746f3151
hatsu38 <hajiwata0308@gmail.com>
2020-08-01 13:00:53 +0900

    Add PageTitle to Link And PageTrans Button to Newlyevents

--- app/javascript/components/NewlyEvents.js
+++ app/javascript/components/NewlyEvents.js
@@ -1,10 +1,15 @@
 import React from "react"
+import PropTypes from 'prop-types'
 import MiniImageEvents from '../components/MiniImageEvents'
 import PageTitle from '../components/PageTitle'
 import Hr from '../components/Hr'
 import { Button } from "react-bootstrap"
 import axios from 'axios'
 const REQUEST_API_BASE_URL = "/api/v1/newly_events"
+
+const propTypes = {
+  hasInfiniteScroll: PropTypes.bool.isRequired,
+}
 
 export default class NewlyEvents extends React.Component {
   constructor(props) {
@@ -62,26 +67,43 @@ export default class NewlyEvents extends React.Component {
 
   render() {
     const { events, isLoading, hasMore, totalEventsCount } = this.state
+    const { hasInfiniteScroll } = this.props
     if(events.length < 1){ return false }
+
+    const moreReadButton = (
+      <div className="moreReadButton--wrapper mb-40">
+        <Button
+          block
+          variant="outline-secondary"
+          className="padding-sm"
+          disabled={isLoading}
+          onClick={isLoading ? null : this.fetchEvents}
+        >
+          {isLoading ? '読み込み中…' : 'もっと見る'}
+        </Button>
+      </div>
+    )
+    const pageTransitionButton = (
+      <div className="moreReadButton--wrapper mb-40">
+        <Button
+          block
+          variant="outline-secondary"
+          className="padding-sm"
+          href="/newly_events"
+        >
+          もっと見る
+        </Button>
+      </div>
+    )
     return (
       <>
-        <PageTitle eventCount={totalEventsCount} title="新着イベント" />
+        <PageTitle eventCount={totalEventsCount} title="新着イベント" link="/newly_events" />
         <Hr />
         {events && <MiniImageEvents events={events} />}
-        {hasMore &&
-          <div className="moreReadButton--wrapper mb-40">
-            <Button
-              block
-              variant="outline-secondary"
-              className="padding-sm"
-              disabled={isLoading}
-              onClick={isLoading ? null : this.fetchEvents}
-            >
-              {isLoading ? '読み込み中…' : 'もっと見る'}
-            </Button>
-          </div>
-        }
+        {hasInfiniteScroll ?　(hasMore && moreReadButton) : pageTransitionButton}
       </>
     )
   }
 }
+
+NewlyEvents.propTypes = propTypes
