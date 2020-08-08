172247bde1212d4906b13f5253b59dc51faa3b6a
hatsu38 <hajiwata0308@gmail.com>
2020-08-08 16:16:17 +0900

    Redirect To www.tech-tv.site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,13 @@ class ApplicationController < ActionController::Base
 
   FQDN = 'www.tech-tv.site'
   # redirect correct server from herokuapp domain for SEO
+  # www.tech-tv.siteへリダイレクトさせる
   def ensure_domain
-    return unless /\.herokuapp.com/ =~ request.host
-
-    # 主にlocalテスト用の対策80と443以外でアクセスされた場合ポート番号をURLに含める
-    port = ":#{request.port}" unless [80, 443].include?(request.port)
-    redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}", status: :moved_permanently
+    if "tech-tv.site" == request.host || /\.herokuapp.com/ =~ request.host
+      # 主にlocalテスト用の対策80と443以外でアクセスされた場合ポート番号をURLに含める
+      port = ":#{request.port}" unless [80, 443].include?(request.port)
+      redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}", status: :moved_permanently
+    end
   end
 
   if Rails.env.production?
