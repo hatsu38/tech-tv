92f72315af3eba82e6c74d37b3a19bf7e3f97f47
hatsu38 <hajiwata0308@gmail.com>
2020-11-25 16:45:38 +0900

    fix by rubocop

--- app/controllers/api/v1/ranking/tags_controller.rb
+++ app/controllers/api/v1/ranking/tags_controller.rb
@@ -5,6 +5,6 @@ class Api::V1::Ranking::TagsController < Api::V1::BaseController
   def index
     tags_num = (params[:tags_num] || DEFAULT_GET_TAGS_NUMS).to_i
     @events_num = (params[:events_num] || DEFAULT_GET_EVENTS_NUMS).to_i
-    @tags = params[:ranking] === "related" ? Tag.related_event_many_order(tags_num) : Event.popular_event_tags(tags_num)
+    @tags = params[:ranking] == "related" ? Tag.related_event_many_order(tags_num) : Event.popular_event_tags(tags_num)
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   # redirect correct server from herokuapp domain for SEO
   # www.tech-tv.siteへリダイレクトさせる
   def ensure_domain
-    if "tech-tv.site" == request.host || /\.herokuapp.com/ =~ request.host
+    if request.host == "tech-tv.site" || /\.herokuapp.com/ =~ request.host
       # 主にlocalテスト用の対策80と443以外でアクセスされた場合ポート番号をURLに含める
       port = ":#{request.port}" unless [80, 443].include?(request.port)
       redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}", status: :moved_permanently
