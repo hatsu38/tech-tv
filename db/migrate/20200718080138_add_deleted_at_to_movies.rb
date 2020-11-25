bbb63980fa846993185907626c3b37e754f826ec
hatsu38 <hajiwata0308@gmail.com>
2020-11-25 16:19:37 +0900

    Fix db/migrate by rubocop

--- db/migrate/20200718080138_add_deleted_at_to_movies.rb
+++ db/migrate/20200718080138_add_deleted_at_to_movies.rb
@@ -1,6 +1,6 @@
 class AddDeletedAtToMovies < ActiveRecord::Migration[6.0]
   def up
-    add_column :movies, :deleted_at, :datetime,  comment: '削除日時'
+    add_column :movies, :deleted_at, :datetime, comment: "削除日時"
   end
 
   def down
